e9eff05f629b51af42b4d2f5242f243f63f283d5
Mubarak Ali <mubarakalis2321@gmail.com>
2024-01-10 04:08:53 +0500

    google auth fully completed2

--- app/Http/Controllers/CustomAuthController.php
+++ app/Http/Controllers/CustomAuthController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AvailableCountry;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class CustomAuthController extends Controller
 {
@@ -35,6 +38,11 @@ class CustomAuthController extends Controller
             $user->email = $data->email;
             $user->email_verified_at = now()->format('Y-m-d H:i:s');
             $user->unique_user_id =  bin2hex(random_bytes(7));
+            $user->secret_key = bin2hex(random_bytes(32));
+            $user->signup_ip = request()->ip();
+            $user->last_ip = request()->ip();
+            $user->country = $this->getCountryCode();
+            $user->upline = $this->getUplineId();
             $user->username = $username;
             $user->password = Hash::make(bin2hex(random_bytes(6)));
 
@@ -45,5 +53,42 @@ class CustomAuthController extends Controller
     
         return redirect('/dashboard'); // Redirect to your dashboard or another route
     }
+
+
+    protected function getCountryCode()
+    {
+        $client = new Client();
+    
+        try {
+            $response = $client->get('https://ipinfo.io/' . request()->ip() . '/country');
+            $countryCode = trim(strtoupper($response->getBody()->getContents()));
+    
+            // Fetch country name from the database
+            $country = AvailableCountry::where('country_code', $countryCode)->value('country_name');
+    
+            return $country;
+        } catch (\GuzzleHttp\Exception\RequestException $e) {
+            // Log or handle the exception
+            dump('Error fetching country code:', $e->getMessage());
+            return null; // Return null or handle the error appropriately
+        }
+    }
+
+    protected function getUplineId(){
+        // Retrieve referral code from the session
+        $referralCode = Session::get('upline_id');
+
+        // Clear the referral code from the session (optional)
+        Session::forget('upline_id');
+        if ($referralCode) {
+            return $referralCode;
+        }
+        else{
+            return 0;
+        }
+
+    }
+
+
     
 }
