ae7bda44e30ca73463e388c520a8ed84ef1dea9c
Mubarak Ali <mubarakalis2321@gmail.com>
2024-01-10 03:48:02 +0500

    google auth fully completed

--- app/Http/Controllers/CustomAuthController.php
+++ app/Http/Controllers/CustomAuthController.php
@@ -12,32 +12,38 @@ class CustomAuthController extends Controller
 {
     public function handleGoogleCallback()
     {
-        $user = Socialite::driver('google')->user();
+        $data = Socialite::driver('google')->user();
         
     
         // Check if the user with this email already exists in your database
         // If not, create a new user record
     
-        $existingUser = User::where('email', $user->email)->first();
-        $nameParts = explode(' ', $user->name);
+        $user = User::where('email', $data->email)->first();
+        $nameParts = explode(' ', $data->name);
         $username = implode('', $nameParts);
-    
-        if ($existingUser) {
-            Auth::login($existingUser);
-        } else {
-            $newUser = new User();
-            $newUser->email = $user->email;
-            $newUser->email_verified_at = now()->format('Y-m-d H:i:s');
-            $newUser->unique_user_id =  bin2hex(random_bytes(7));
-            $newUser->username = $username;
-            $newUser->password = Hash::make(bin2hex(random_bytes(6)));
-
-            $newUser->save;
-    
-            Auth::login($newUser);
+        $usernameExists = User::where('username', $username)->first();
+        //if username exists, add a 4 char random string next to it
+        if($usernameExists){
+            $username = $username.rand(1000,9999);
         }
     
-        return redirect()->route('dashboard'); // Redirect to your dashboard or another route
+        if ($user) {
+            Auth::login($user);
+        } else {
+            $user = new User();
+            $user->name = $data->name;
+            $user->email = $data->email;
+            $user->email_verified_at = now()->format('Y-m-d H:i:s');
+            $user->unique_user_id =  bin2hex(random_bytes(7));
+            $user->username = $username;
+            $user->password = Hash::make(bin2hex(random_bytes(6)));
+
+            $user->save();
+    
+            Auth::login($user);
+        }
+    
+        return redirect('/dashboard'); // Redirect to your dashboard or another route
     }
     
 }
