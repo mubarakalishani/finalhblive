e3d34f2922df79777eb62fc02bf5f9f30b3bd8d2
mubarakalis2321@gmail.com <adminhb@mail.handbucks.com>
2024-01-17 19:05:06 +0000

    completed some changes from admin panel

--- app/Admin/Controllers/OffersAndSurveysLogsController.php
+++ app/Admin/Controllers/OffersAndSurveysLogsController.php
@@ -31,6 +31,7 @@ class OffersAndSurveysLogsController extends AdminController
         $grid->column('provider_name', __('Provider name'));
         $grid->column('payout', __('Payout'));
         $grid->column('reward', __('Reward'));
+        $grid->column('added_expert_level', __('Added expert level'));
         $grid->column('upline_commision', __('Upline commision'));
         $grid->column('transaction_id', __('Transaction id'));
         $grid->column('offer_id', __('Offer id'));
@@ -38,6 +39,7 @@ class OffersAndSurveysLogsController extends AdminController
         $grid->column('hold_time', __('Hold time'));
         $grid->column('instant_credit', __('Instant credit'));
         $grid->column('ip_address', __('Ip address'));
+        $grid->column('status', __('Status'));
         $grid->column('created_at', __('Created at'));
         $grid->column('updated_at', __('Updated at'));
 
@@ -59,6 +61,7 @@ class OffersAndSurveysLogsController extends AdminController
         $show->field('provider_name', __('Provider name'));
         $show->field('payout', __('Payout'));
         $show->field('reward', __('Reward'));
+        $show->field('added_expert_level', __('Added expert level'));
         $show->field('upline_commision', __('Upline commision'));
         $show->field('transaction_id', __('Transaction id'));
         $show->field('offer_id', __('Offer id'));
@@ -66,6 +69,7 @@ class OffersAndSurveysLogsController extends AdminController
         $show->field('hold_time', __('Hold time'));
         $show->field('instant_credit', __('Instant credit'));
         $show->field('ip_address', __('Ip address'));
+        $show->field('status', __('Status'));
         $show->field('created_at', __('Created at'));
         $show->field('updated_at', __('Updated at'));
 
@@ -85,6 +89,7 @@ class OffersAndSurveysLogsController extends AdminController
         $form->text('provider_name', __('Provider name'));
         $form->decimal('payout', __('Payout'))->default(0.0000);
         $form->decimal('reward', __('Reward'))->default(0.0000);
+        $form->decimal('added_expert_level', __('Added expert level'))->default(0.0000);
         $form->decimal('upline_commision', __('Upline commision'))->default(0.0000);
         $form->text('transaction_id', __('Transaction id'));
         $form->text('offer_id', __('Offer id'));
@@ -92,6 +97,7 @@ class OffersAndSurveysLogsController extends AdminController
         $form->number('hold_time', __('Hold time'));
         $form->switch('instant_credit', __('Instant credit'));
         $form->text('ip_address', __('Ip address'));
+        $form->switch('status', __('Status'));
 
         return $form;
     }
