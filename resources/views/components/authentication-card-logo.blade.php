18be5c90f04253ef54aef69d778c01c1ff47084e
Mubarak Ali <mubarakalis2321@gmail.com>
2024-01-21 02:14:30 +0500

    changed background of logo

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,3 +1,3 @@
 <a href="/">
-    <img src="images/logo.png" width="200px"/>
+    <img src="images/logo.png" width="200px" style="background-color: white; border-radius:8px; margin-top:20px;"/>
 </a>
