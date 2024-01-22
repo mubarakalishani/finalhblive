d8374400e702032beff3892dfe716591976496a5
Mubarak Ali <mubarakalis2321@gmail.com>
2024-01-23 01:13:22 +0500

    fixed dashboard account section

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -314,7 +314,7 @@
                     <div class="splide__track">
                         <div class="splide__list">
                           @foreach ($offerwalls as $offerwall)
-                            <div class="col splide__slide m-0" style="min-width: 200px">
+                            <div class="col splide__slide m-0" style="min-width: 150px">
                                 <a @if($offerwall->is_target_blank !=0 ) target="_blank" @endif
                                   class="offerwall-button" data-toggle="modal" data-target="#myModal" data-header="{{ $offerwall->name }}" data-url="{{ $offerwall->url }}">
                                   <div class="item inner-item">
