4712eb29248e801981d2674e64e5bbc5c46f4c3d
Mubarak Ali <mubarakalis2321@gmail.com>
2024-01-26 22:07:56 +0500

    fixed that wrong offers history header and offers page

--- resources/views/worker/all-offers.blade.php
+++ resources/views/worker/all-offers.blade.php
@@ -7,7 +7,7 @@
         <div class="container">
             <div class="row">
                 @foreach ($offerwalls as $offerwall)
-                    <div class="col-lg-2 col-md-3 col-sm-12">
+                    <div class="col-lg-2 col-md-3 col-sm-12 mt-3">
                         <a @if($offerwall->is_target_blank !=0 ) target="_blank" @endif
                             class="offerwall-button" data-toggle="modal" data-target="#myModal" data-header="{{ $offerwall->name }}" data-url="{{ $offerwall->iframe_url }}">
                             <img width="100px" src="{{$offerwall->image_url}}" alt="{{$offerwall->name}}">
