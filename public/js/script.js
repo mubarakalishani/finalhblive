d74b7ac3c95f056b2fcb6b543c934388a8754ea9
Mubarak Ali <mubarakalis2321@gmail.com>
2024-01-26 19:52:15 +0500

    Added tawk.to

--- public/js/script.js
+++ public/js/script.js
@@ -37,3 +37,15 @@ var splide1 = new Splide('.splide1', {
                 },
             });
             splide1.mount();
+
+
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/65b3c6590ff6374032c53c55/1hl33tkdj';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
