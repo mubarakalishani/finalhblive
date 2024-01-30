e2ab78cc68ad143da264fb9b9a940ff7fa92a5b4
Mubarak Ali <mubarakalis2321@gmail.com>
2024-01-30 23:37:08 +0500

    added releasing at show at offers and surveys history page

--- public/js/script.js
+++ public/js/script.js
@@ -4,13 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Display or hide the sidebar based on the screen width
     if (windowWidth <= 767) {
-        const sidebar = document.getElementById('sidebar');
-        const main = document.getElementById('main');
-        const footer = document.getElementById('footer');
-
-        sidebar.classList.toggle('closed');
-        main.classList.toggle('closed');
-        footer.classList.toggle('closed');
+        document.querySelector("#sidebar").classList.toggle("collapsed");
     }
 });
 
