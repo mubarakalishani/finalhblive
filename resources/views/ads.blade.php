4662d5c38c7beabe8f7dfa0bba4d4c90067c91ac
Mubarak Ali <mubarakalis2321@gmail.com>
2024-02-11 02:02:17 +0500

    users with less than or equal to 0 balance cannot create tasks

---
+++ resources/views/ads.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <iframe data-aa='2301916' src='//ad.a-ads.com/2301916?size=240x400' style='width:240px; height:400px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>
+
+    </div>
+    <div>
+        <iframe data-aa='2301917' src='//ad.a-ads.com/2301917?size=240x400' style='width:240px; height:400px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>
+
+    </div>
+</body>
+</html>
