4298bfbba6a879cf8427504ff10f4b243201e207
Mubarak Ali <mubarakalis2321@gmail.com>
2024-02-13 19:39:46 +0500

    added payment proofs full page

--- resources/views/pages/payment-proofs.blade.php
+++ resources/views/pages/payment-proofs.blade.php
@@ -22,7 +22,7 @@
                 </td>
                 <td>
                   <span class="text-info">
-                    <img style="width: 150px; height:40px;" src="{{ $withdrawal->image }}" >
+                    <img style="width: 100px; height:25px;" src="{{ $withdrawal->image }}" >
                   </span>
                 </td>
                 <td>
