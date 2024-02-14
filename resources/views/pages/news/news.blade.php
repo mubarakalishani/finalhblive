caed04c648b5e9044fd6de54d706e18e8d2049bd
Mubarak Ali <mubarakalis2321@gmail.com>
2024-02-14 22:14:17 +0500

    added news and announcements page

--- resources/views/pages/news/news.blade.php
+++ resources/views/pages/news/news.blade.php
@@ -1,12 +1,12 @@
 @include('includes.header-before-login')
 <div class="container mt-3 mb-3">
     <div class="row">
-        <div class="col-lg-8 col-sm-12 col-md-12" style="   border-left: 2px solid aliceblue;">
+        <div class="col-lg-8 col-sm-12 col-md-12">
             <h5>{{ $latestNews->title }}</h5>
             <p>Last Updated: {{ $latestNews->updated_at->diffForHumans() }}</p>
             {!! $latestNews->content !!}
         </div>
-        <div class="col-lg-4 col-sm-12 col-md 12">
+        <div class="col-lg-4 col-sm-12 col-md 12" style="border-left: 2px solid aliceblue;">
             <h6 class="mt-3 mb-3">More Guides and Announcements:</h6>
             @foreach ($news as $item)
                 <ul>
