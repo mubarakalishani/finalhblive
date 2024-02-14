9001b517f786f537842eb2d472bcd1e5ead8885a
Mubarak Ali <mubarakalis2321@gmail.com>
2024-02-14 22:21:48 +0500

    added news and announcements page

--- resources/views/pages/news/news-full-page.blade.php
+++ resources/views/pages/news/news-full-page.blade.php
@@ -1,12 +1,12 @@
 @include('includes.header-before-login')
 <div class="container mt-3 mb-3">
-    <div class="row most-popular-1">
+    <div class="row" style="padding: 30px;background-color: #E6F3F8;border-radius: 10px;">
         <div class="col-lg-8 col-sm-12 col-md-12">
             <h4>{{ $newsToShow->title }}</h4>
             <p>Last Updated: {{ $newsToShow->updated_at->diffForHumans() }}</p>
             {!! $newsToShow->content !!}
         </div>
-        <div class="col-lg-4 col-sm-12 col-md 12" style="   border-left: 2px solid aliceblue;">
+        <div class="col-lg-4 col-sm-12 col-md 12" style="border-left: 2px solid aliceblue;">
             <h6 class="mt-3 mb-3">More Guides and Announcements:</h6>
             @foreach ($news as $item)
                 <ul>
