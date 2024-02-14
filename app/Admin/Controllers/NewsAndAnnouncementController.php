9aaaa37ee9f670f461c6e6d4a6052587d4f7b7cc
mubarakalis2321@gmail.com <adminhb@mail.handbucks.com>
2024-02-14 15:00:40 +0000

    added news

---
+++ app/Admin/Controllers/NewsAndAnnouncementController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use OpenAdmin\Admin\Controllers\AdminController;
+use OpenAdmin\Admin\Form;
+use OpenAdmin\Admin\Grid;
+use OpenAdmin\Admin\Show;
+use \App\Models\NewsAndAnnouncement;
+
+class NewsAndAnnouncementController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'NewsAndAnnouncement';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new NewsAndAnnouncement());
+
+        $grid->column('id', __('Id'));
+        $grid->column('type', __('Type'));
+        $grid->column('title', __('Title'));
+        $grid->column('content', __('Content'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(NewsAndAnnouncement::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('type', __('Type'));
+        $show->field('title', __('Title'));
+        $show->field('content', __('Content'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new NewsAndAnnouncement());
+
+        $form->switch('type', __('Type'));
+        $form->text('title', __('Title'));
+        $form->textarea('content', __('Content'));
+
+        return $form;
+    }
+}
