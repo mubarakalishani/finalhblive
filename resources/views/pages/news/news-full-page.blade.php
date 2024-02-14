524c71714c81bdda7745399e1e73daed310e9bf8
Mubarak Ali <mubarakalis2321@gmail.com>
2024-02-14 21:09:45 +0500

    added news and announcements page

--- resources/views/pages/news/news-full-page.blade.php
+++ resources/views/pages/news/news-full-page.blade.php
@@ -1,19 +1,15 @@
 @include('includes.header-before-login')
 <div class="container mt-3 mb-3">
-    
-    <div class="content-wrapper border p-3">
-        <h4>{{ $newsToShow->title }}</h4>
-        <p>Last Updated: {{ $newsToShow->updated_at->diffForHumans() }}</p>
-        {!! $newsToShow->content !!}
-
-        <h6 class="mt-3 mb-3">More Guides and Announcements:</h6>
-        @foreach ($news as $item)
-            <ul>
-                <li>
-                    <a href="/guides-and-announcements/{{ $item->id }}">{{ $item->title }}</a> <br>
-                </li>
-            </ul>
-        @endforeach
-    </div>
+    <h4>{{ $newsToShow->title }}</h4>
+    <p>Last Updated: {{ $newsToShow->updated_at->diffForHumans() }}</p>
+    {!! $newsToShow->content !!}
+    <h6 class="mt-3 mb-3">More Guides and Announcements:</h6>
+    @foreach ($news as $item)
+        <ul>
+            <li>
+                <a href="/guides-and-announcements/{{ $item->id }}">{{ $item->title }}</a> <br>
+            </li>
+        </ul>
+    @endforeach
 </div>
 @include('includes.footer-before-login')
