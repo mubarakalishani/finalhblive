721acc42af2e4481c385c125bd024f25a3724219
Mubarak Ali <mubarakalis2321@gmail.com>
2024-02-18 20:17:04 +0500

    added stat route in admin panel

--- resources/views/admin/stats.blade.php
+++ resources/views/admin/stats.blade.php
@@ -1,42 +1,42 @@
 <div class="row">
-    <div class="col-lg-4 col-md-6 col-sm-6">
+    <div class="col-lg-3 col-md-4 col-sm-6">
         <h4>Deposits</h4>
         <p>Total: <b></b></p>
         <p>This Month: <b></b></p>
         <p>Last Month: <b></b></p>
     </div>
-    <div class="col-lg-4 col-md-6 col-sm-6">
+    <div class="col-lg-3 col-md-4 col-sm-6">
         <h4>Payments</h4>
         <p>Pending: <b></b></p>
         <p>Total: <b></b></p>
         <p>This Month: <b></b></p>
         <p>Last Month: <b></b></p>
     </div>
-    <div class="col-lg-4 col-md-6 col-sm-6">
+    <div class="col-lg-3 col-md-4 col-sm-6">
         <h4>Users Earning</h4>
         <p>Offerwalls: <b></b></p>
         <p>Shortlinks: <b></b></p>
         <p>PTC: <b></b></p>
         <p>Taks: <b></b></p>
     </div>
-</div>
-
-<div class="row">
-    <div class="col-lg-4 col-md-6 col-sm-6">
+    <div class="col-lg-3 col-md-4 col-sm-6">
         <h4>Tasks</h4>
         <p>pending Approval: <b></b></p>
         <p>Total Earned: <b></b></p>
         <p>This Month: <b></b></p>
         <p>Last Month: <b></b></p>
     </div>
-    <div class="col-lg-4 col-md-6 col-sm-6">
+</div>
+
+<div class="row">
+    <div class="col-lg-3 col-md-4 col-sm-6">
         <h4>Offers</h4>
         <p>Today: <b></b></p>
         <p>total: <b></b></p>
         <p>This Month: <b></b></p>
         <p>Last Month: <b></b></p>
     </div>
-    <div class="col-lg-4 col-md-6 col-sm-6">
+    <div class="col-lg-3 col-md-4 col-sm-6">
         <h4>Shortlinks</h4>
         <p>Total: <b></b></p>
         <p>Today: <b></b></p>
@@ -44,18 +44,15 @@
         <p>Last Month: <b></b></p>
     </div>
 
-</div>
-
-
-<div class="row">
-    <div class="col-lg-4 col-md-6 col-sm-6">
+    <div class="col-lg-3 col-md-4 col-sm-6">
         <h4>PTC</h4>
         <p>pending Approval: <b></b></p>
         <p>Total Earned: <b></b></p>
         <p>This Month: <b></b></p>
         <p>Last Month: <b></b></p>
     </div>
-    <div class="col-lg-4 col-md-6 col-sm-6">
+
+    <div class="col-lg-3 col-md-4 col-sm-6">
         <h4>Faucets</h4>
         <p>Total: <b></b></p>
         <p>Today: <b></b></p>
