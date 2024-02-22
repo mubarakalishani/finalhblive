47f8f9e828af1ecf323189f363c11fa22dc7251d
Mubarak Ali <mubarakalis2321@gmail.com>
2024-02-23 01:20:46 +0500

    improved admin panel's filter options

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -266,12 +266,6 @@
                       </div>
                   </div>
                 </div>
-
-                <div class="mt-5 text-center" style="border: 1px solid floralwhite;border-radius: 10px;">
-                  <p>Advertisement</p>
-                  <iframe data-aa='2301691' src='//ad.a-ads.com/2301691?size=728x90' style='width:728px; height:90px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>
-                </div>
-                
               </div>
               <!-- ***** Most Popular Provider End ***** -->
               <!-- ***** Most Popular payment gateways start ***** -->
@@ -325,10 +319,6 @@
                     </div>
                   </div>
                 </div>
-              </div>
-              <div class="mt-5 text-center" style="border: 1px solid floralwhite;border-radius: 10px;">
-                <p>Advertisement</p>
-                <iframe data-aa='2301815' src='//ad.a-ads.com/2301815?size=468x60' style='width:468px; height:60px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>
               </div>
               <!-- ***** Most Popular payment gateways End ***** -->
               <!-- ***** Withdraw and task history Start ***** -->
