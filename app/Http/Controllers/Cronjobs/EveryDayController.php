50c3b72728fe3b8d0f60d89dc59b7bbe14173c53
Mubarak Ali <mubarakalis2321@gmail.com>
2024-02-23 15:06:30 +0500

    added daily cron

--- app/Http/Controllers/Cronjobs/EveryDayController.php
+++ app/Http/Controllers/Cronjobs/EveryDayController.php
@@ -16,8 +16,8 @@ class EveryDayController extends Controller
         $resubmitExhaustedProofs = SubmittedTaskProof::where('status', 7)->get();
         foreach ($resubmitExhaustedProofs as $resubmitExhaustedProof) {
             $task= Task::find($resubmitExhaustedProof->task_id);
-            $employer = User::where($task->employer_id);
-            dd($employer);
+            $employer = User::find($task->employer_id);
+            dd('employer id is ' . $employer->id);
             $resubmitExhaustedProof->update([
                 'status' => 7,
             ]);
