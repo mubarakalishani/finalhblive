aa104661cc893730d43876e0817fa4e8b518a7a8
Mubarak Ali <mubarakalis2321@gmail.com>
2024-02-24 18:33:43 +0500

    added delays to redirection of windows ptc submit

--- app/Http/Controllers/Worker/PtcAdController.php
+++ app/Http/Controllers/Worker/PtcAdController.php
@@ -321,7 +321,7 @@ class PtcAdController extends Controller
                     'description' => 'received Referral commission from '.$worker->username.' for completing a task'
                 ]);
             }
-            return redirect(url('/views/window'))->with('success', $ad->reward_per_view.' added to your balance successfully');
+            return redirect(url('/views/window'))->with('success', $ad->reward_per_view.' added to your balance successfully')->withDelay(2);
         // }else{
         //     return back()->with('error', 'Invalid Captcha');
         // }
