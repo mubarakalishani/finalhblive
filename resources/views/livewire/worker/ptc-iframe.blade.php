82d20766e17c89472da47b1155dd5efde2f45e3e
Mubarak Ali <mubarakalis2321@gmail.com>
2024-03-01 02:31:27 +0500

    fixed dashboard col size

--- resources/views/livewire/worker/ptc-iframe.blade.php
+++ resources/views/livewire/worker/ptc-iframe.blade.php
@@ -1,7 +1,13 @@
 <div>
-    <div>
-      <h4 id="safeTimerDisplay">Loading:</4>
-    </div>
+    <header class="header-area">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid  mx-2">
+            <a href="/" class="navbar-brand">
+                <img src="/images/logo.png"  alt="CoolBrand" style="">
+            </a>
+            <strong><span id="safeTimerDisplay">Loading:</span></strong>
+      </nav>
+    </header>
     <iframe id="adFrame" src="{{ $url }}" width="100%" height="1000px"></iframe>
 
     @livewireScripts
