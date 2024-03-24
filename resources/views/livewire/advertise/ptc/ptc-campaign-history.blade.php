67cc2fd956c9703e6d023ab8983ceb815d2e1bbf
Mubarak Ali <mubarakalis2321@gmail.com>
2024-03-25 03:44:47 +0500

    added countdown to faucet

--- resources/views/livewire/advertise/ptc/ptc-campaign-history.blade.php
+++ resources/views/livewire/advertise/ptc/ptc-campaign-history.blade.php
@@ -154,7 +154,7 @@
     </div>
 
 
-    <div class="modal fade" id="confirmstop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal fade" id="confirmstop" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
           <div class="modal-content">
             <div class="modal-header">
