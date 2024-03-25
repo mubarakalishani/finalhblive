ed1db9f9144b0fbee54d24a23ebc1e384a62058b
mubarakalis2321@gmail.com <adminhb@mail.handbucks.com>
2024-03-25 14:15:06 +0000

    added some admin files from server end

---
+++ app/Admin/Controllers/AdminProofDisputeController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use OpenAdmin\Admin\Controllers\AdminController;
+use OpenAdmin\Admin\Form;
+use OpenAdmin\Admin\Grid;
+use OpenAdmin\Admin\Show;
+use \App\Models\AdminProofDispute;
+
+class AdminProofDisputeController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'AdminProofDispute';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new AdminProofDispute());
+
+        $grid->column('id', __('Id'));
+        $grid->column('user_id', __('User id'));
+        $grid->column('employer_id', __('Employer id'));
+        $grid->column('task_id', __('Task id'));
+        $grid->column('proof_id', __('Proof id'));
+        $grid->column('description', __('Description'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(AdminProofDispute::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('user_id', __('User id'));
+        $show->field('employer_id', __('Employer id'));
+        $show->field('task_id', __('Task id'));
+        $show->field('proof_id', __('Proof id'));
+        $show->field('description', __('Description'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new AdminProofDispute());
+
+        $form->number('user_id', __('User id'));
+        $form->number('employer_id', __('Employer id'));
+        $form->number('task_id', __('Task id'));
+        $form->number('proof_id', __('Proof id'));
+        $form->textarea('description', __('Description'));
+
+        return $form;
+    }
+}
---
+++ app/Admin/Controllers/TaskTargetedCountriesController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use OpenAdmin\Admin\Controllers\AdminController;
+use OpenAdmin\Admin\Form;
+use OpenAdmin\Admin\Grid;
+use OpenAdmin\Admin\Show;
+use \App\Models\TaskTargetedCountry;
+
+class TaskTargetedCountriesController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'TaskTargetedCountry';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new TaskTargetedCountry());
+
+        $grid->column('id', __('Id'));
+        $grid->column('task_id', __('Task id'));
+        $grid->column('country', __('Country'));
+        $grid->column('amount_per_task', __('Amount per task'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(TaskTargetedCountry::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('task_id', __('Task id'));
+        $show->field('country', __('Country'));
+        $show->field('amount_per_task', __('Amount per task'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new TaskTargetedCountry());
+
+        $form->number('task_id', __('Task id'));
+        $form->text('country', __('Country'));
+        $form->decimal('amount_per_task', __('Amount per task'))->default(0.000);
+
+        return $form;
+    }
+}
